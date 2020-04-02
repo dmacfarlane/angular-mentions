0dee8d49fbaf4987bf5e4caf4c6ac5513e315eef
Sarah Miranda <smiranda@you-source.com>
2020-04-02 11:21:56 +0800

    update component from parent repo

--- src/mention/mention-list.component.ts
+++ src/mention/mention-list.component.ts
@@ -1,13 +1,13 @@
 import {
-  Component, ElementRef, Output, EventEmitter, ViewChild, ContentChild, Input,
-  TemplateRef, OnInit
+  Component, ElementRef, Output, EventEmitter, ViewChild, Input,
+  TemplateRef, AfterContentChecked
 } from '@angular/core';
 
 import { isInputOrTextAreaElement, getContentEditableCaretCoords } from './mention-utils';
 import { getCaretCoordinates } from './caret-coords';
 
 /**
- * Angular 2 Mentions.
+ * Angular Mentions.
  * https://github.com/dmacfarlane/angular-mentions
  *
  * Copyright (c) 2016 Dan MacFarlane
@@ -43,7 +43,7 @@ import { getCaretCoordinates } from './caret-coords';
     </ul>
     `
 })
-export class MentionListComponent implements OnInit {
+export class MentionListComponent implements AfterContentChecked {
   @Input() labelKey: string = 'label';
   @Input() itemTemplate: TemplateRef<any>;
   @Output() itemClick = new EventEmitter();
@@ -52,9 +52,12 @@ export class MentionListComponent implements OnInit {
   items = [];
   activeIndex: number = 0;
   hidden: boolean = false;
-  constructor(private _element: ElementRef) {}
+  dropUp: boolean = false;
+  private coords: {top:number, left:number} = {top:0, left:0};
+  private offset: number = 0;
+  constructor(private element: ElementRef) {}
 
-  ngOnInit() {
+  ngAfterContentChecked() {
     if (!this.itemTemplate) {
       this.itemTemplate = this.defaultItemTemplate;
     }
@@ -62,33 +65,30 @@ export class MentionListComponent implements OnInit {
 
   // lots of confusion here between relative coordinates and containers
   position(nativeParentElement: HTMLInputElement, iframe: HTMLIFrameElement = null) {
-    let coords = { top: 0, left: 0 };
     if (isInputOrTextAreaElement(nativeParentElement)) {
       // parent elements need to have postition:relative for this to work correctly?
-      coords = getCaretCoordinates(nativeParentElement, nativeParentElement.selectionStart);
-      coords.top = nativeParentElement.offsetTop + coords.top + 16;
-      coords.left = nativeParentElement.offsetLeft + coords.left;
+      this.coords = getCaretCoordinates(nativeParentElement, nativeParentElement.selectionStart, null);
+      this.coords.top = nativeParentElement.offsetTop + this.coords.top - nativeParentElement.scrollTop;
+      this.coords.left = nativeParentElement.offsetLeft + this.coords.left - nativeParentElement.scrollLeft;
+      // getCretCoordinates() for text/input elements needs an additional offset to position the list correctly
+      this.offset = this.getBlockCursorDimensions(nativeParentElement).height;
     }
     else if (iframe) {
       let context: { iframe: HTMLIFrameElement, parent: Element } = { iframe: iframe, parent: iframe.offsetParent };
-      coords = getContentEditableCaretCoords(context);
+      this.coords = getContentEditableCaretCoords(context);
     }
     else {
       let doc = document.documentElement;
       let scrollLeft = (window.pageXOffset || doc.scrollLeft) - (doc.clientLeft || 0);
       let scrollTop = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
-
       // bounding rectangles are relative to view, offsets are relative to container?
       let caretRelativeToView = getContentEditableCaretCoords({ iframe: iframe });
       let parentRelativeToContainer: ClientRect = nativeParentElement.getBoundingClientRect();
-
-      coords.top = caretRelativeToView.top - parentRelativeToContainer.top + nativeParentElement.offsetTop - scrollTop;
-      coords.left = caretRelativeToView.left - parentRelativeToContainer.left + nativeParentElement.offsetLeft - scrollLeft;
+      this.coords.top = caretRelativeToView.top - parentRelativeToContainer.top + nativeParentElement.offsetTop - scrollTop;
+      this.coords.left = caretRelativeToView.left - parentRelativeToContainer.left + nativeParentElement.offsetLeft - scrollLeft;
     }
-    let el: HTMLElement = this._element.nativeElement;
-    el.style.position = "absolute";
-    el.style.left = coords.left + 'px';
-    el.style.top = coords.top + 'px';
+    // set the default/inital position
+    this.positionElement();
   }
 
   get activeItem() {
@@ -129,7 +129,47 @@ export class MentionListComponent implements OnInit {
     this.activeIndex = Math.max(Math.min(this.activeIndex - 1, this.items.length - 1), 0);
   }
 
-  resetScroll() {
+  // reset for a new mention search
+  reset() {
     this.list.nativeElement.scrollTop = 0;
+    this.checkBounds();
+  }
+
+  // final positioning is done after the list is shown (and the height and width are known)
+  // ensure it's in the page bounds
+  private checkBounds() {
+    let left = this.coords.left, top = this.coords.top, dropUp = this.dropUp;
+    const bounds: ClientRect = this.list.nativeElement.getBoundingClientRect();
+    // if off right of page, align right
+    if (bounds.left + bounds.width > window.innerWidth) {
+      left -= bounds.left + bounds.width - window.innerWidth + 10;
+    }
+    // if more than half off the bottom of the page, force dropUp
+    // if ((bounds.top+bounds.height/2)>window.innerHeight) {
+    //   dropUp = true;
+    // }
+    // if top is off page, disable dropUp
+    if (bounds.top<0) {
+      dropUp = false;
+    }
+    // set the revised/final position
+    this.positionElement(left, top, dropUp);
+  }
+
+  private positionElement(left:number=this.coords.left, top:number=this.coords.top, dropUp:boolean=this.dropUp) {
+    const el: HTMLElement = this.element.nativeElement;
+    top += dropUp ? 0 : this.offset; // top of list is next line
+    el.className = dropUp ? 'dropup' : null;
+    el.style.position = "absolute";
+    el.style.left = left + 'px';
+    el.style.top = top + 'px';
+  }
+
+  private getBlockCursorDimensions(nativeParentElement: HTMLInputElement) {
+    const parentStyles = window.getComputedStyle(nativeParentElement);
+    return {
+      height: parseFloat(parentStyles.lineHeight),
+      width: parseFloat(parentStyles.fontSize)
+    };
   }
 }
